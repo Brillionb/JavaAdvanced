5cf982c9fae0d7daae33e9e97e0ee592c0f7a0b2
Galina Magazinova <gmagazinova@gmail.com>
2023-11-18 16:16:17 +0300

    add ArrayDequeDemo

---
+++ src/main/java/com/galina_magazinova/javacore/chapter18/ArrayDequeDemo.java
@@ -0,0 +1,24 @@
+package main.java.com.galina_magazinova.javacore.chapter18;
+// Демонстрация использования ArrayDeque
+
+import java.util.*;
+
+public class ArrayDequeDemo {
+    public static void main(String[] args) {
+        // создать ArrayDeque
+        ArrayDeque<String> adq = new ArrayDeque<>();
+
+        // использовать экземпляр ArrayDeque подобно стеку
+        adq.push("A");
+        adq.push("B");
+        adq.push("D");
+        adq.push("E");
+        adq.push("F");
+
+        System.out.print("Извлечение из стека: ");
+        while(adq.peek() != null) {
+            System.out.print(adq.pop() + " ");
+        }
+        System.out.println();
+    }
+}
